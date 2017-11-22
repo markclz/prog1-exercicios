935386cf420d09b14a69a4c1f882238845a6c585
Marcos Vinícius <marcosc@id.uff.br>
2017-11-22 20:42:41 -0200

    questao 2 lista 4

---
+++ q2l4.py
@@ -0,0 +1,14 @@
+n = int(input("Digite a quantidade de elementos do vetor:"))
+k = int(input("Digite o valor da chave K: "))
+vet =[0] * n
+pos = "Não encontrado!"
+
+for i in range(n):
+    vet[i] = int(input("Digite o valor para posição: " + str(i) + ": "))
+print(vet)
+
+for i in range(len(vet)):
+    if vet[i] == k:
+     pos = i
+print("Posição: ",pos)
+
