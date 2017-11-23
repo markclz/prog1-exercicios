5a87425b84cf91a060f918f58838b06ea762390e
Marcos Vinícius <marcosc@id.uff.br>
2017-11-23 20:02:19 -0200

    questao 2 lista 4

---
+++ q3l4.py
@@ -0,0 +1,35 @@
+n = int(input("Digite a quantidade de elementos do vetor:"))
+vet = [0] * n
+#vet2 =[]
+for i in range(n):
+    vet[i] = int(input("Digite o valor do elemento da posição " + str(i) + ":"))
+print(vet)
+print(len(vet))
+
+"""
+vet2 = vet
+print(vet2)
+
+for i in range(len(vet)):
+    if vet[i] == i:
+        print(i)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for i in range(len(vet)):
+
+
+
+
---
+++ q4l3.py
@@ -0,0 +1,20 @@
+# cria um vetor com 50 numeros inteiros de 1 a 50
+
+a = 0
+v = []
+vr = []
+for i in range(50):
+    a = a + 1
+    v.append(a)
+print(v)
+
+# verifica dentro do vetor com os 50 elementos quais correspondem a um dia do mes
+for i in range(len(v)):
+    if v[i] > 0 and v[i] <= 31:
+        vr.append(v[i])
+print("Vetor Resultante:",vr)
+
+
+
+
+
