4321e747df1a85b9e19453db079891f147339d55
Marcos Vinícius <marcosc@id.uff.br>
2017-11-29 10:58:55 -0200

    questao 5 lista 4

---
+++ q5l4.py
@@ -0,0 +1,14 @@
+
+n = int(input("Digite o tamanho do vetor: "))
+vet = []
+for i in range(n):
+    vet.append(int(input("Preencher vetor: ")))
+print("Conjunto A =",vet)
+print("Numero de elementos: ",len(vet))
+soma = 0
+for i in range(n//2):
+    soma = soma + (vet[i] - vet[n-1-i]) ** 2
+print("A soma é:",soma)
+
+
+
