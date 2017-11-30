d31eceeeffbd6b705647872f42b99d105d7dd8c1
Marcos Vinícius <marcosc@id.uff.br>
2017-11-30 10:29:41 -0200

    questao 6 lista 4 (com reprocessamento) tem que formatar as casas, pois a saida do 0.4 esta 1.200000 e la vai zeros

--- q6l4.py
+++ q6l4.py
@@ -1,17 +1,22 @@
-n = int(input("Digite a quantidade de elementos do conjunto: "))
-vet = []
-for i in range(n):
-    vet.append(float(input("Digite os elementos do conjunto A:")))
-print(vet)
-print("O tamanho do vetor é:",len(vet))
-b = [0] * n
-for i in range(len(vet)):
-    if i % 2 == 1:
-        b[i] = vet[i]/2
-    else:
-        i + 1 % 2 == 0
-        b[i] = vet[i] * 3
-print("B:",b)
+a = "s"
+while a!= "n":
+    n = int(input("Digite a quantidade de elementos do conjunto: "))
+    vet = []
+    for i in range(n):
+        vet.append(float(input("Digite os elementos do conjunto A:")))
+    print(vet)
+    print("O tamanho do vetor é:",len(vet))
+    b = [0] * n
+    for i in range(len(vet)):
+        if i % 2 == 1:
+            b[i] = vet[i]/2
+        else:
+            i + 1 % 2 == 0
+            b[i] = vet[i] * 3
+    print("B:",b)
+    print("Deseja relizar uma nova operação?(s/n)")
+    a = input()
+print("fim do programa")
 
 
 
