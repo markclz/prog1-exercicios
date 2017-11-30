dc045509370ae657d99f7339a39ff4589c9d6f9b
Marcos Vinícius <marcosc@id.uff.br>
2017-11-30 20:20:40 -0200

    questa 7 lista 4, menor e maior elemento de um vetor

---
+++ q7l4.py
@@ -0,0 +1,31 @@
+n = int(input("Digite o numero de elementos do vetor: "))
+vet = [0] * n
+c = 0
+for i in range(n):
+    c+=1
+    vet[i] = c # em vez de aplicar o incremento da variavel c para preencer o vetor com as 25 posições podeira usar um input e o usuario ir colocando
+print(vet)
+maior = 0
+j = 0
+
+# verifica qual é o maior elemento
+while j < n:
+    if vet[j] > maior:
+        maior = vet[j]
+    j = j + 1
+print("O maior elemento é:", maior)
+
+# verifica qual é o menor elemento
+menor = 25
+k = 0
+while k < n:
+    if vet[k] < menor:
+        menor = vet[k]
+    k = k + 1
+print("O menor elemento é:", menor)
+
+
+
+
+
+
--- rascunho l4.py
+++ rascunho l4.py
@@ -1,38 +1,19 @@
-n = int(input("Digite o numero de alunos:"))
+
+
+n = int(input("Numero de elementos do vetor: "))
+c = 0
+while c < n:
+    c = c + 1
+    p1 = float(input("Digite a nota da P1 do aluno: "))
+    p2 = float(input("Digite a nota da P2 do aluno: "))
+    media = (p1+p2)/2
+
 vet = [0] * n
-
 for i in range(n):
-media = (p1+p2) / 2
+    vet[i] = media
+print(vet)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if vet[i] >=
 
 
