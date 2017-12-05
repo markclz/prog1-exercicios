6986f3412dc7622bde36dad9524a5a541684223c
Marcos Vinícius <marcosc@id.uff.br>
2017-12-05 16:52:29 -0200

    string inversao da palavra uiversidade

---
+++ inversao de strings detalhadamente, com a palavra universidade.py
@@ -0,0 +1,12 @@
+p = "Universidade"
+nv = ""
+for i in range(len(p)-1,-1,-1):
+    print("Decrescente %d"%i)
+    nv+= p[i]
+print(nv)
+print()
+nv = ""
+for i in range(len(p)):
+    print("crescente %d, decrescente %d" %(i,len(p)-1-i))
+    nv+=p[len(p)-1-i]
+print(nv)
